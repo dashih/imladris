9fd9be40e49d58eafa054ff88b589807971757a5
dss4f <dss4f@virginia.edu>
2015-11-14 02:58:31 -0800

    Actually commit time sync script.

---
+++ config/timesync.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo 'Sync system time with nist.gov...'
+/usr/sbin/ntpdate time.nist.gov >> ~/timesync.log
+
+echo 'Sync hardware clock with system time...'
+/sbin/hwclock --systohc
+/sbin/hwclock --show
+
+echo '--------';
+
+exit 0
