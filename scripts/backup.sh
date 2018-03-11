dcea8c247e70407e8190173faf6bca68c69f439e
dss4f <danny@dannyshih.net>
2018-03-10 23:43:08 -0800

    Updates backup.sh script and crontab.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,47 +1,17 @@
 #!/bin/sh
 
-if [ "$1" = '' ]
-then
-    echo 'Gotta supply an argument.'
-    exit 0
-fi
+echo '---------- START -----------'
+/bin/date
+echo '----------------------------'
 
-if [ "$1" != '--test' ]
-then
-    echo '--------------------------------------------------------------------------------' &> "$1/backup.log"
-    date >> "$1/backup.log"
-    echo '--------------------------------------------------------------------------------' >> "$1/backup.log"
-fi
-
-backupDirs=(
-    '/home/www'
-    '/home/basement/music'
-    '/home/basement/pictures'
-    '/home/basement/software'
-    '/home/dss4f'
-)
-
-size=0
-for dir in "${backupDirs[@]}"
-do
-    dirSize=$(($(du -sb $dir | cut -f1)))
-    if [ "$1" = '--test' ]
-    then
-        echo $dir":" $(numfmt --to=si $dirSize)
-    else
-        rsync -av --progress $dir $1
-        echo $dir":" $(numfmt --to=si $dirSize) >> "$1/backup.log"
-    fi
-    
-    size=$(($size+$dirSize))
-done
-
-if [ "$1" = '--test' ]
-then
-    echo -e "\nTOTAL:" $(numfmt --to=si $size)
+if /usr/bin/mount | /usr/bin/grep /mnt/backup > /dev/null; then
+    /usr/bin/rsync -avh /home /mnt/backup
 else
-    echo -e "\nTOTAL:" $(numfmt --to=si $size) >> "$1/backup.log"
-    echo "Log written to $1/backup.log"
+    echo 'Backup disk is not mounted. Aborting.'
 fi
+
+echo '----------- END ------------'
+/bin/date
+echo '----------------------------'
 
 exit 0
