8e940b77b3b4a1697a21f597a3684c562a64363f
dss4f <danny@dannyshih.net>
2018-04-01 14:08:13 -0700

    Changes archive directory so that it's a subdirectory in the backup mount. Also addes the --delete rsync option, so stale stuff is cleaned up.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,7 +5,7 @@ echo '---------- START -----------'
 echo '----------------------------'
 
 if /usr/bin/mount | /usr/bin/grep /mnt/backup > /dev/null; then
-    /usr/bin/rsync -avh /home /mnt/backup
+    /usr/bin/rsync -avh --delete /home /mnt/backup/archive
 else
     echo 'Backup disk(s) are not mounted. Aborting.'
 fi
