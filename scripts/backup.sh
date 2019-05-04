32473b796b23b79a68ba43017654215bd3723949
dss4f <danny@dannyshih.net>
2019-05-04 16:56:58 -0700

    Fixes backup.sh to exclude dovecot directories. These were failing due to permissions issues and need not be backed up anyway.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -5,7 +5,9 @@ echo '---------- START -----------'
 echo '----------------------------'
 
 if /usr/bin/mount | /usr/bin/grep /mnt/dannyshih_net_backup > /dev/null; then
-    /usr/bin/rsync -avh --delete /home /mnt/dannyshih_net_backup
+    /usr/bin/rsync -avh --delete --exclude '*Maildir*' --exclude '*mail_data*' /home /mnt/dannyshih_net_backup
+    echo '========'
+    /usr/bin/du -hs /mnt/dannyshih_net_backup
 else
     echo 'Backup disk(s) are not mounted. Aborting.'
 fi
