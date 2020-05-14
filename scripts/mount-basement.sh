93d00be5c0375ec3f919820b053ef25a201e6573
dss4f <danny@dannyshih.net>
2020-05-14 00:46:52 -0700

    Scripts for mounting shares.

---
+++ scripts/mount-basement.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo mount.cifs -o vers=3.0,username=dss4f //192.168.1.64/basement /mnt/basement
+
+exit 0
---
+++ scripts/mount-dannyshih_net_backup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo /usr/sbin/mount.nfs4 192.168.1.64:/dannyshih_net_backup /mnt/dannyshih_net_backup
+
+exit 0
