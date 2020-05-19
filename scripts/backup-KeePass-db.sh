ac767b19e15959e287c96987b3c83bcf6cf7953f
dss4f <danny@dannyshih.net>
2020-05-19 13:35:47 -0700

    KeePass script update

--- scripts/backup-KeePass-db.sh
+++ scripts/backup-KeePass-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget -O /home/dss4f/KeePass-Backup/tmp.kdbx "https://onedrive.live.com/download?cid=48AAA6D8292CB685&resid=48AAA6D8292CB685%21202&authkey=ANtSwsRYhm93Vno"
-fileName="lunboks.`date +"%m-%d-%y"`.kdbx"
+wget -O /home/dss4f/KeePass-Backup/tmp.kdbx "https://onedrive.live.com/download?cid=48AAA6D8292CB685&resid=48AAA6D8292CB685%21227&authkey=ANJRXUfPd9_qrks"
+fileName="secrets.`date +"%m-%d-%y"`.kdbx"
 mv /home/dss4f/KeePass-Backup/tmp.kdbx /home/dss4f/KeePass-Backup/$fileName
 echo "Renamed backup to $fileName"
 
