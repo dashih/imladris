585c80eb0bae526fe61775b2aac7ac10604dc6f1
dss4f <danny@dannyshih.net>
2020-05-19 22:35:43 -0700

    Yet another KeePass change.

--- scripts/backup-KeePass-db.sh
+++ scripts/backup-KeePass-db.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wget -O /home/dss4f/KeePass-Backup/tmp.kdbx "https://onedrive.live.com/download?cid=48AAA6D8292CB685&resid=48AAA6D8292CB685%21227&authkey=ANJRXUfPd9_qrks"
-fileName="secrets.`date +"%m-%d-%y"`.kdbx"
+wget -O /home/dss4f/KeePass-Backup/tmp.kdbx "https://onedrive.live.com/download?cid=48AAA6D8292CB685&resid=48AAA6D8292CB685%21228&authkey=AGIZmMyBqL93FBU"
+fileName="gringotts.`date +"%m-%d-%y"`.kdbx"
 mv /home/dss4f/KeePass-Backup/tmp.kdbx /home/dss4f/KeePass-Backup/$fileName
 echo "Renamed backup to $fileName"
 
