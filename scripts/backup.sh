6276043145c02a2ff5f10dcae9759987b1b8f43f
dss4f <danny@dannyshih.net>
2020-05-27 11:52:30 -0700

    Overhauls the backup.sh script. Now, all data is primarily in Nextcloud. We only need to backup gas, gringotts backup, and the Nextcloud database.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -4,10 +4,20 @@ echo '---------- START -----------'
 /bin/date
 echo '----------------------------'
 
-if /usr/bin/mount | /usr/bin/grep /mnt/dannyshih_net_backup > /dev/null; then
-    /usr/bin/rsync -avh --delete --exclude '*Maildir*' --exclude '*mail_data*' --exclude '*.git*' /home /mnt/dannyshih_net_backup
-    echo '========'
-    /usr/bin/du -hs /mnt/dannyshih_net_backup
+if /usr/bin/mount | /usr/bin/grep /mnt/dannyshih_net_backup > /dev/null
+then
+    newdbFileName="`openssl rand -hex 12`.sql.gz"
+    newdbFile="/mnt/dannyshih_net_backup/nextcloud-db-backups/$newdbFileName"
+    mariadb-dump --user=nextcloudbackup --lock-tables --databases nextcloud | gzip -9 &> $newdbFile
+    echo "Nextcloud database backup: $newdbFileName"
+
+    echo -e '\n========\n'
+
+    rsync -avh /home/dss4f/gas/ /mnt/dannyshih_net_backup/gas
+    rsync -avh /home/dss4f/gringotts-backup/ /mnt/dannyshih_net_backup/gringotts-backup
+
+    echo -e '\n========\n'
+    du -hs /mnt/dannyshih_net_backup
 else
     echo 'Backup disk(s) are not mounted. Aborting.'
 fi
