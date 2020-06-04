1bc5aa15559e8933534f88c259cabe7b46c5974d
dss4f <dss4f@dannyshih.net>
2020-06-04 14:20:17 -0700

    Adds quotes to backup.

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,8 +9,13 @@ if /usr/bin/mount | /usr/bin/grep /mnt/dannyshih_net_backup > /dev/null
 then
     tmpDir=`mktemp -d`
     backupArchive="`echo $tmpDir | cut -d'.' -f2`.tar.gz"
+
     mariadb-dump --user=nextcloudbackup --lock-tables --databases nextcloud &> $tmpDir/nextcloud.sql
-    mongodump --quiet --db=gas --username=gasbackup --password="KEYHtwFz@MCcE5nam8n6b3z#mjLbFhJGs@TnUn6#JUVHKWFJF9twKszjZHf2@rEfQpg4tLWMLajCCL9kvPrLxAWCy9Vi9VKxq9UEbgJ#n4MX5a8#u9YT" --out=$tmpDir
+
+    mongodump --quiet --db=gas --username=gasbackup --password='KEYHtwFz@MCcE5nam8n6b3z#mjLbFhJGs@TnUn6#JUVHKWFJF9twKszjZHf2@rEfQpg4tLWMLajCCL9kvPrLxAWCy9Vi9VKxq9UEbgJ#n4MX5a8#u9YT' --out=$tmpDir
+
+    mongodump --quiet --db=quotes --username=quotesbackup --password='R9N6j2Zb#28oeTSZ9CqR5KH$X2cHjJfWyc7M@4EVgUv7T@yGR' --out=$tmpDir
+
     tar -czf /mnt/dannyshih_net_backup/db-backups/$backupArchive --directory $tmpDir .
     rm -rf $tmpDir
     echo "Database backups archive: $backupArchive"
