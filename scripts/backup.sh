83a5836560459b31ae6ee9c714ddd7154ae5eae9
dss4f <dss4f@dannyshih.net>
2020-06-04 20:21:19 -0700

    backup.sh - Adds backup of Nextcloud config

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -10,12 +10,26 @@ then
     tmpDir=`mktemp -d`
     backupArchive="`echo $tmpDir | cut -d'.' -f2`.tar.gz"
 
-    mariadb-dump --user=nextcloudbackup --lock-tables --databases nextcloud &> $tmpDir/nextcloud.sql
+    # Nextcloud
+    echo "Backing up Nextcloud..."
+    nextcloudInstallDir=/var/www/html/nextcloud
+    nextcloudBackupDir=$tmpDir/nextcloud
+    mkdir $nextcloudBackupDir
+    sudo -u apache php $nextcloudInstallDir/occ maintenance:mode --on
+    mariadb-dump --single-transaction --user=nextcloudbackup nextcloud &> $nextcloudBackupDir/nextcloud.sql
+    sudo cp $nextcloudInstallDir/config/config.php $nextcloudBackupDir
+    sudo chmod a+r $nextcloudBackupDir/config.php
+    sudo -u apache php $nextcloudInstallDir/occ maintenance:mode --off
 
+    # Gas
+    echo "Backing up gas..."
     mongodump --quiet --db=gas --username=gasbackup --password='KEYHtwFz@MCcE5nam8n6b3z#mjLbFhJGs@TnUn6#JUVHKWFJF9twKszjZHf2@rEfQpg4tLWMLajCCL9kvPrLxAWCy9Vi9VKxq9UEbgJ#n4MX5a8#u9YT' --out=$tmpDir
 
+    # Quotes
+    echo "Backing up quotes..."
     mongodump --quiet --db=quotes --username=quotesbackup --password='R9N6j2Zb#28oeTSZ9CqR5KH$X2cHjJfWyc7M@4EVgUv7T@yGR' --out=$tmpDir
 
+    # Archive all db backups together
     tar -czf /mnt/dannyshih_net_backup/db-backups/$backupArchive --directory $tmpDir .
     rm -rf $tmpDir
     echo "Database backups archive: $backupArchive"
