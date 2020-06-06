c14f712a249139be5f733475a9b9c360f36b9893
dss4f <dss4f@dannyshih.net>
2020-06-06 02:02:24 -0700

    Consolidate cron jobs into user.

--- scripts/check_nas_raid.js
+++ scripts/check_nas_raid.js
@@ -11,8 +11,6 @@ exec("ssh root@192.168.1.64 'mdadm --detail /dev/md2'", (err, stdout, stderr) =>
 
     let m = stdout.match(/[\s\S]*State : ([a-z]+)\s/);
     let status = m[1];
-
-
     if (status != "clean" || status != "active") {
         exec(util.format("echo -e \"%s\" | mailx -s \"NAS RAID not healthy!\" dss4f@dannyshih.net", stdout), (errEmail, stdoutEmail, stderrEmail) => {
             if (errEmail != null) {
