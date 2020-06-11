c77e5b5c328c8061ef57800c5e758c4ed9f08b9d
dss4f <dss4f@dannyshih.net>
2020-06-10 23:19:00 -0700

    Adds provision to the renew-certs logic to properly set letsencrypt permissions.

--- scripts/start-services.sh
+++ scripts/start-services.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Set permissions so dss4f can access the new cert.
+/usr/bin/chgrp -R ssl /etc/letsencrypt
+/usr/bin/chmod -R g+rX /etc/letsencrypt
+
 /bin/systemctl start danny-gas-app
 /bin/systemctl start httpd
 /bin/systemctl start postfix
