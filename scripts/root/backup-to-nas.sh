022d99b30a711bb7933db2a96b8874d83eaa656a
dss4f <dss4f@dannyshih.net>
2020-06-16 15:45:27 -0700

    Modifies backup-to-nas script to not check the external NAS raid array via ssh. ssh keys get cleared on reboots, and the ZyXEL NAS beeps if degraded anyway.  Also removes dynamic progress output from the rsync command.

--- scripts/root/backup-to-nas.sh
+++ scripts/root/backup-to-nas.sh
@@ -7,9 +7,7 @@ echo ''
 
 if /usr/bin/mount | /usr/bin/grep /mnt/nas > /dev/null
 then
-    rsync -avh --no-owner --no-group --no-perms --delete --progress /mnt/raid/ /mnt/nas
-    echo -e '\n----------------------------\n'
-    ssh -i /home/dss4f/.ssh/id_ed25519 root@192.168.1.64 'mdadm --detail /dev/md2'
+    rsync -avh --no-owner --no-group --no-perms --delete /mnt/raid/ /mnt/nas
     echo -e '\n----------------------------\n'
     mdadm --detail /dev/md127
 else
