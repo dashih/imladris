44ae33bf608fc8907dddfb35771618bcb8836121
dss4f <dss4f@dannyshih.net>
2020-07-06 16:16:13 -0700

    Makes start/stop-services log to stdout.

--- scripts/root/start-services.sh
+++ scripts/root/start-services.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-echo "Setting permissions on /etc/letsencrypt..." 1>&2
+echo "Setting permissions on /etc/letsencrypt..."
 /usr/bin/chgrp -R ssl /etc/letsencrypt
 /usr/bin/chmod -R g+rX /etc/letsencrypt
 
-echo "Starting services..." 1>&2
+echo "Starting services..."
 /bin/systemctl start danny-gas-app
 /bin/systemctl start danny-quotes
 /bin/systemctl start httpd
 /bin/systemctl start postfix
 /bin/systemctl start dovecot
-echo "Done." 1>&2
+echo "Done."
 
 exit 0
--- scripts/root/stop-services.sh
+++ scripts/root/stop-services.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-echo "Stopping services for SSL cert renewal..." 1>&2
+echo "Stopping services for SSL cert renewal..."
 /bin/systemctl stop danny-gas-app
 /bin/systemctl stop danny-quotes
 /bin/systemctl stop httpd
 /bin/systemctl stop dovecot
 /bin/systemctl stop postfix
-echo "Yielding to certbot for cert renewal..." 1>&2
+echo "Yielding to certbot for cert renewal..."
 
 exit 0
