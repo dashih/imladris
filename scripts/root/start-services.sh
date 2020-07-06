072021ec7feb66232c5dbbe4b141ef2de7419d91
dss4f <dss4f@dannyshih.net>
2020-07-06 00:04:12 -0700

    Changes start/stop services to output to stderr so we get the email during cron execution.

--- scripts/root/start-services.sh
+++ scripts/root/start-services.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-# Set permissions so dss4f can access the new cert.
+echo "Setting permissions on /etc/letsencrypt..." 1>&2
 /usr/bin/chgrp -R ssl /etc/letsencrypt
 /usr/bin/chmod -R g+rX /etc/letsencrypt
 
+echo "Starting services..." 1>&2
 /bin/systemctl start danny-gas-app
 /bin/systemctl start danny-quotes
 /bin/systemctl start httpd
 /bin/systemctl start postfix
 /bin/systemctl start dovecot
+echo "Done." 1>&2
 
 exit 0
--- scripts/root/stop-services.sh
+++ scripts/root/stop-services.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+echo "Stopping services for SSL cert renewal..." 1>&2
 /bin/systemctl stop danny-gas-app
 /bin/systemctl stop danny-quotes
 /bin/systemctl stop httpd
 /bin/systemctl stop dovecot
 /bin/systemctl stop postfix
+echo "Yielding to certbot for cert renewal..." 1>&2
 
 exit 0
